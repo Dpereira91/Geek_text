e14e5065166bc29771850c43c00ce26ef130af7d
scace006 <scace006@fiu.edu>
2017-06-26 21:41:42 -0400

    New refresh containing book table

--- geek_text.sql
+++ geek_text.sql
@@ -21,7 +21,9 @@ SET time_zone = "+00:00";
 --
 -- Database: `geek_text`
 --
-
+DROP DATABASE IF EXISTS geek_text;
+CREATE DATABASE geek_text;
+USE geek_text;
 -- --------------------------------------------------------
 
 --
@@ -118,6 +120,33 @@ INSERT INTO `user` (`id`, `password`, `name`, `nickname`, `avatar`, `email`, `st
 ('dpere326', 'gebxt2oUKU50A', 'Darilys Pereira', 'Dari', 2, 'darilys1991@yahoo.com', '3421 NW Flalger', 'none', 'Miami', 'Fl', '33125', 'null'),
 ('mcabrera1', 'gebxt2oUKU50A', 'Mari Cab', 'yula', 6, 'mcabre@yahoo.com', '3453 SW 3123th Ct', ' ', 'Miami', 'Fl', '33183', 'null');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `genre` varchar(15) NOT NULL,
+  `rating` float NOT NULL,
+  `price` varchar(6) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`id`, `title`, `description`, `genre`, `rating`, `price`) VALUES
+(0, 'Dale\'s Tales', 'A brief history of Dale\'s life.', 'Biography', 5, '10.10'),
+(1, 'Cesar Salads', 'Health and fitness expert, Cesar, shares all his fitness secrets.', 'Health', 4.5, '9.00'),
+(2, 'Nicolas\' Life', 'Non-existent.', 'Fiction', 5, 'Free'),
+(3, 'Generic Book', 'The generic book for non-readers.', 'General', 5, '15.00'),
+(4, 'Book 1', 'Description 1', 'Horror', 4.2, '8.50'),
+(5, 'The life of Pi', '3.141 59265 358 9793 2384626 433 83 279 50288 41 971 6939 93 7 510.', 'Education', 3.14, '3.14');
+
 --
 -- Indexes for dumped tables
 --
@@ -139,6 +168,13 @@ ALTER TABLE `shipping`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
+  
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
