16d578d2eaace08fa5ea5c33e874da037c2ac27a
scace006 <scace006@fiu.edu>
2017-06-26 21:39:29 -0400

    Update books.sql
    Added prices

--- books.sql
+++ books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2017 at 08:14 PM
+-- Generation Time: Jun 27, 2017 at 01:26 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -25,27 +25,27 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `books`
 --
-USE geek_text;
 
 CREATE TABLE `books` (
   `id` int(11) NOT NULL,
   `title` varchar(30) NOT NULL,
   `description` varchar(250) NOT NULL,
   `genre` varchar(15) NOT NULL,
-  `rating` float NOT NULL
+  `rating` float NOT NULL,
+  `price` varchar(6) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`id`, `title`, `description`, `genre`, `rating`) VALUES
-(0, 'Dale\'s Tales', 'A brief history of Dale\'s life.', 'Biography', 5),
-(1, 'Cesar Salads', 'Health and fitness expert, Cesar, shares all his fitness secrets.', 'Health', 4.5),
-(2, 'Nicolas\' Life', 'Non-existent.', 'Fiction', 5),
-(3, 'Generic Book', 'The generic book for non-readers.', 'General', 5),
-(4, 'Book 1', 'Description 1', 'Horror', 4.2),
-(5, 'The life of Pi', '3.141 59265 358 9793 2384626 433 83 279 50288 41 971 6939 93 7 510.', 'Education', 3.14);
+INSERT INTO `books` (`id`, `title`, `description`, `genre`, `rating`, `price`) VALUES
+(0, 'Dale\'s Tales', 'A brief history of Dale\'s life.', 'Biography', 5, '10.10'),
+(1, 'Cesar Salads', 'Health and fitness expert, Cesar, shares all his fitness secrets.', 'Health', 4.5, '9.00'),
+(2, 'Nicolas\' Life', 'Non-existent.', 'Fiction', 5, 'Free'),
+(3, 'Generic Book', 'The generic book for non-readers.', 'General', 5, '15.00'),
+(4, 'Book 1', 'Description 1', 'Horror', 4.2, '8.50'),
+(5, 'The life of Pi', '3.141 59265 358 9793 2384626 433 83 279 50288 41 971 6939 93 7 510.', 'Education', 3.14, '3.14');
 
 --
 -- Indexes for dumped tables
