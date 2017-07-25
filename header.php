f3b5472792f2e37978670034f2d00a156e65fc87
cvill033 <cvill033@fiu.edu>
2017-07-25 19:05:01 -0400

    Updated the header to include the logo, logo1.png under images

--- header.php
+++ header.php
@@ -11,7 +11,8 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" style="color:chartreuse; font-size: 24;" href="./">GeekText</a>
+      <img src="images/logo1.png" style="width:250px;height:50px;">
+      <a class="navbar-brand" style="color:chartreuse; font-size: 24;" href="./"></a>
     </div>
     <ul class="nav navbar-nav">
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Browse Books<span class="caret"></span></a>
