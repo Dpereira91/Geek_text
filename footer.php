bcbf8888c6327cb867632720c330433b6ffca0e5
cvill033 <cvill033@fiu.edu>
2017-07-25 19:22:21 -0400

    Added The Last Borders Logo (images/logo2.png), tweaked the spacing a bit

--- footer.php
+++ footer.php
@@ -7,13 +7,15 @@
 <br>
 <br>
 <br>
-<hr style="border-width:2px;">
+<hr style="border-width:3px;">
+<center><img src="images/logo2.png" style="width:180px;height:100px;"></center>
 <p class= "footerbottom">
-	© 2017 The Last Borders Company | 
-	<a href="./contact.php">Contact Us</a> | 
-	<a href="./terms.php">Terms of Service</a> | 
-	<a href="./privacy.php">Privacy Policy</a> | 
+	© 2017 The Last Borders Company |  
+	<a href="./contact.php">Contact Us</a> |  
+	<a href="./terms.php">Terms of Service</a> |  
+	<a href="./privacy.php">Privacy Policy</a> |  
 	<a href="./help.php">Help</a>
 </p>
+<br>
 </body>
 </html>
