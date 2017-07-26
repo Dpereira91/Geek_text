aeb0cd0904104c58afea338b2a773567452ead0b
cvill033 <cvill033@fiu.edu>
2017-07-25 20:33:08 -0400

    Improved formatting, justified text

--- index.php
+++ index.php
@@ -8,20 +8,17 @@
 <?php include("header.php");?>
 
 <p><img src="images/banner_new.png" style="margin-top:-30px; max-width:100%;"></p>
-
-<p style="text-align: center;"><b>Geek Text</b></p>
 <br>
-
-<p style="text-align:center; font-family:courier-new; font-size:4em;">TECH VALLEY TIMES BEST SELLERS</p>
-<p style="text-align: center;"><b>By Tech Valley Times</b><br>JULY 26, 2017</p>
-<p style="text-align: center; padding-left:20%; padding-right:15%; font-size:1.2em;">
+<br>
+<p style="text-align: center; font-size:2em">By <b> Tech Valley Times</b> &nbsp | &nbsp July 26, 2017</p>
+<p style="text-align: justify; padding-left:30%; padding-right:30%; font-size:1.2em;">
 	It's that time of year again: the weather is getting chillier, the days are getting 
 	shorter, and there's nothing better than snuggling up and getting a good read. Even 
 	if you haven't picked up a book since The Fourth of July, fear not; we've gone 
 	through all the best titles and come up with a few best sellers	that will keep you 
 	entertained. Happy reading!
 </p>
-
+<br>
 <?php
 	$link = mysqli_connect("localhost", "root", "", "geek_text");
 	$sql = "SELECT * FROM books ORDER BY rating DESC LIMIT 10";
