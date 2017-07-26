36354a746aa1f605576f0aee706709d62db0b413
cvill033 <cvill033@fiu.edu>
2017-07-25 21:18:31 -0400

    Updated formatting, changed button

--- contact.php
+++ contact.php
@@ -7,17 +7,20 @@
 
 <?php include("header.php");?>
 
-<p style="margin-left:20; font-size:3em;">Let Us Know How We Can Help</p>
-<h3 style="margin-left:20;">Contact Information:</h3>
+<center>
+<p style="font-size:3.5em;"><b>Contact Us</b></p>
+<hr>
 <br>
-<p style="margin-left:40; font-size:1.2em;">send us an email<br>CustomerRelations@Gtext.com</p>
+<p style="font-size:2em;"><b>Send us an Email</b><br></p>
+<p style="font-size:1.5em;">CustomerRelations@Gtext.com<br></p>
 <br>
-<p style="margin-left:40; font-size:1.2em;">give us a call<br>1-800-440-0680<br>for relay service, please dial 711</p>
+<p style="font-size:2em;"><b>Give us a Call</b><br></p>
+<p style="font-size:1.5em;">1-800-440-0680<br></p>
 <br>
-<p style="margin-left:40; font-size:1.2em;">chat with us now<br><button style="color:white; background-color:red; padding:10px 24px; border-radius:8px;">chat now</button></p>
-<br>
+<p style="font-size:2em;"><b>Chat with us</b><br></p>
+<button style="color:black; background-color:#ffc600; padding:10px 24px; border-radius:8px;"><b>Chat Now</b></button>
+</center>
 
 <?php include("footer.php");?>
-
 </body>
 </html>
